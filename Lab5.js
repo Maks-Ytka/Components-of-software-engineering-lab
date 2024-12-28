5f12088199a586dbaa541bede37a643731ead92f
Maks-Ytka <143877967+Maks-Ytka@users.noreply.github.com>
2024-12-28 05:21:23 +0200

    added registerMessageHandler method to handle message events

--- Lab5.js
+++ Lab5.js
@@ -32,4 +32,23 @@ class ReactiveEntity extends EventEmitter {
         console.log(`${this.name} отримує повідомлення від ${from}: "${message}"`);
     }
 }
+class ReactiveEntity extends EventEmitter {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+
+    sendMessage(target, message) {
+        console.log(`${this.name} відправляє повідомлення до ${target.name}: "${message}"`);
+        target.emit('message', this.name, message);
+    }
+
+    receiveMessage(from, message) {
+        console.log(`${this.name} отримує повідомлення від ${from}: "${message}"`);
+    }
+
+    registerMessageHandler() {
+        this.on('message', (from, message) => this.receiveMessage(from, message));
+    }
+}
 
