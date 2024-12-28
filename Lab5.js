2a6db3a50bee24651c9eb17d63657bab73692b3e
Maks-Ytka <143877967+Maks-Ytka@users.noreply.github.com>
2024-12-28 05:22:02 +0200

    added an example of using ReactiveEntity for messaging

--- Lab5.js
+++ Lab5.js
@@ -51,4 +51,16 @@ class ReactiveEntity extends EventEmitter {
         this.on('message', (from, message) => this.receiveMessage(from, message));
     }
 }
+const entityA = new ReactiveEntity('Entity A');
+const entityB = new ReactiveEntity('Entity B');
+const entityC = new ReactiveEntity('Entity C');
+
+entityA.registerMessageHandler();
+entityB.registerMessageHandler();
+entityC.registerMessageHandler();
+
+entityA.sendMessage(entityB, 'Привіт, Entity B!');
+entityB.sendMessage(entityA, 'Привіт, Entity A! Як справи?');
+entityC.sendMessage(entityA, 'Привіт, Entity A! Це Entity C.');
+entityA.sendMessage(entityC, 'Привіт, Entity C! Рад тебе чути.');
 
