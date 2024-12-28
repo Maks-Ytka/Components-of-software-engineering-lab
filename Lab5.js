f3c1fae8fd4988c662a169a2969491571060290b
Maks-Ytka <143877967+Maks-Ytka@users.noreply.github.com>
2024-12-28 05:20:35 +0200

    added sendMessage method for sending messages

--- Lab5.js
+++ Lab5.js
@@ -6,4 +6,15 @@ class ReactiveEntity extends EventEmitter {
         this.name = name;
     }
 }
+class ReactiveEntity extends EventEmitter {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+
+    sendMessage(target, message) {
+        console.log(`${this.name} відправляє повідомлення до ${target.name}: "${message}"`);
+        target.emit('message', this.name, message);
+    }
+}
 
